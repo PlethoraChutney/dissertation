bacd1cf5ffb62e3c38bef52ceb7f295328661818
Rich <rich@posert.com>
2023-08-02 10:17:04 -0700

    Start on overview frontmatter

--- replace_title_page.py
+++ replace_title_page.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python
 from PyPDF2 import PdfWriter, PageRange
 from pathlib import Path
+import sys
 
 merger = PdfWriter()
 
-with open('title-page.pdf', 'rb') as f:
+with open("title-page.pdf", "rb") as f:
     merger.append(f)
 
-dissertation_location = Path('_book', 'posert-dissertation.pdf')
-with open(dissertation_location, 'rb') as f:
-    merger.append(f, pages=PageRange('1:'))
+dissertation_location = Path("_book", "posert-dissertation.pdf")
+try:
+    with open(dissertation_location, "rb") as f:
+        merger.append(f, pages=PageRange("1:"))
+except FileNotFoundError:
+    sys.exit()
 
 merger.write(dissertation_location)
