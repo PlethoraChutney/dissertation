50309bddc255a9a6b0078f7584cb304207d0f3a1
Rich <rich@posert.com>
2023-08-17 15:09:21 -0700

    Overview done, start glossary

--- replace_title_page.py
+++ replace_title_page.py
@@ -1,14 +1,25 @@
 #!/usr/bin/env python
-from PyPDF2 import PdfWriter, PageRange
+from PyPDF2 import PdfWriter, PdfReader, PageRange
 from pathlib import Path
 import sys
+
+# check if we've already added the title page
+
+dissertation_location = Path("_book", "posert-dissertation.pdf")
+try:
+    with open(dissertation_location, "rb") as f:
+        dissertation = PdfReader(f)
+        should_be_acknowledgements = dissertation.pages[2].extract_text()
+        if "For Lindsey, who shows me" in should_be_acknowledgements:
+            sys.exit()
+except FileNotFoundError:
+    sys.exit()
 
 merger = PdfWriter()
 
 with open("title-page.pdf", "rb") as f:
     merger.append(f)
 
-dissertation_location = Path("_book", "posert-dissertation.pdf")
 try:
     with open(dissertation_location, "rb") as f:
         merger.append(f, pages=PageRange("1:"))
---
+++ sort_glossary.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+from pathlib import Path
+
+glossary_path = Path("parts", "glossary.qmd")
+
+with open(glossary_path, "r") as f:
+    glossary_lines = [x.rstrip() for x in f]
+
+glossary_lines = glossary_lines[2:]
+glossary = {}
+
+for i in range(len(glossary_lines)):
+    line = glossary_lines[i]
+    if ": " in line:
+        glossary[glossary_lines[i - 1]] = line
+
+sorted_keys = list(glossary.keys())
+sorted_keys.sort()
+
+with open(glossary_path, "w") as f:
+    f.write("# Glossary {.unnumbered}\n")
+    f.write("\n")
+    for key in sorted_keys:
+        f.write(key + "\n")
+        f.write(glossary[key] + "\n")
+        f.write("\n")
